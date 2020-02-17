e94dcdbef54ba607d28a5f9d84aa0f3008a8de3e
Vincent Servio <vincentservio@gmail.com>
2020-02-17 21:51:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,13 @@
 class Dog 
+    def name=(new_name)
+        @name = new_name
+end
 
-def initialize(name)
-  @name = name 
-end 
 
   def name 
     @name 
   end 
-      def name=(new_name)
-        @name = new_name
-end
+    
 end
 fido = Dog.new
 fido.name("Fido")
