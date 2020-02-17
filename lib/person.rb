bc06d889c6aaab094f094c5643f60327d7300c4e
Vincent Servio <vincentservio@gmail.com>
2020-02-17 22:02:51 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+  def name=(new_person)
+    @name = new_person
+  end 
+  def name 
+    @name 
+  end 
+  def job=(new_job)
+    @job = new_job
+  end
+  def job 
+    @job 
+end 
+end
