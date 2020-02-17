81520f23350fb011365d609a995c4c3292e9ca7a
Vincent Servio <vincentservio@gmail.com>
2020-02-17 20:41:16 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+# def bark 
+#   puts "woof!"
+#   end
+#   def name=(dog_name)
+#     @the_dogs_name = dog_name
+    
+#   end
+#   def name 
+#     @the_dogs_name
+#   end
+end 
+
+lassie = Dog.new
+lassie.name=("Lassie")
+ 
+lassie.name
